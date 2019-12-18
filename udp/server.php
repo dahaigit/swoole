b7fe649c62c0509536019e721174c49e93c98cc9
menghailong <menghailong@xiyuemedia.com>
2019-12-18 15:34:21 +0800

    Revert "Initial commit"
    This reverts commit 6ea06adcaafaad985ad5a35582bafaf13e2c60aa.

--- udp/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-$udpServer = new swoole_server('127.0.0.1', 8081, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
-
-$udpServer->on('Packet', function($udpServer, $data, $clientInfo){
-        $udpServer->sendto($clientInfo['address'], $clientInfo['port'], 'UDP server'. $data);
-        var_dump($clientInfo);
-});
-
-$udpServer->start();
